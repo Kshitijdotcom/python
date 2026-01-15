b4b30e758ce61b28a9fce8f16f64a94d0b6fca4c
Kshitijdotcom <kshitijgharat12@gmail.com>
2026-01-15 12:25:33 +0530

    git add index.html git commit -m "Fix: Delete button now works with single click - removed duplicate clearImage() function" git push

--- utils/init_db.py
+++
@@ -1,8 +0,0 @@
-"""Initialize the database with correct schema"""
-from image_editor_server import app, db
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-        print("Database initialized successfully!")
-        print("You can now start your server.")
